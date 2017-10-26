37ad3ead8de666f3267f0b9ec04ce37eadfeff09
alexkonovalov <alvkonovalov@gmail.com>
2017-10-26 23:30:09 +0300

    feat(weather): reduce code duplication in reducer

--- src/app/weather/weather.reducer.ts
+++ src/app/weather/weather.reducer.ts
@@ -13,6 +13,35 @@ export const initialState: WeatherState = {
 
 export const selectorWeather = state => state.weather;
 
+export function weatherReducer(state: WeatherState = initialState, action: WeatherActionTypes): WeatherState {
+  switch (action.type) {
+
+    case ACTION_KEYS.WEATHER_RETRIEVE: {
+      return { ...state,
+        IsFluxLoading: true,
+        IsCloudsLoading: true
+      };
+    }
+
+    case ACTION_KEYS.FLUX_RETRIEVE_SUCCESS: {
+      return { ...state,
+        ...updateWithInfos(action.by, state, val => ({Flux: val})),
+        IsFluxLoading: false
+      };
+    }
+
+    case ACTION_KEYS.CLOUDNESS_RETRIEVE_SUCCESS: {
+      return { ...state,
+        ...updateWithInfos(action.by, state, val => ({Cloud: val})),
+        IsCloudsLoading: false
+      };
+    }
+
+    default:
+      return state;
+  }
+}
+
 function getDayInfos(infos: WeatherInfo[]): {value: number, day: moment.Moment, time: moment.Moment}[] {
   return infos
     .map(info => ({value: info.value, date: moment.utc(info.time)}))
@@ -23,7 +52,7 @@ function getDayInfos(infos: WeatherInfo[]): {value: number, day: moment.Moment, 
     }));
 }
 
-function mergeDateArrays(arr1: moment.Moment[], arr2: moment.Moment[]): moment.Moment[] {
+function mergeToDistinctDates(arr1: moment.Moment[], arr2: moment.Moment[]): moment.Moment[] {
   return Array
     .from(new Set([...arr1
         .map(day => day.unix()),
@@ -33,77 +62,30 @@ function mergeDateArrays(arr1: moment.Moment[], arr2: moment.Moment[]): moment.M
     .map(day => moment.utc(moment.unix(day)));
 }
 
-export function weatherReducer(state: WeatherState = initialState, action: WeatherActionTypes): WeatherState {
-  switch (action.type) {
+function updateWithInfos(weatherInfos: WeatherInfo[],
+    state: WeatherState,
+    callback: (value: number) => { Flux: number } | { Cloud: number }) {
 
-    case ACTION_KEYS.WEATHER_RETRIEVE: {
-      return {
-        ...state,
-        IsFluxLoading: true,
-        IsCloudsLoading: true
-      };
-    }
+  const dayInfos = getDayInfos(weatherInfos);
+  const dates = dayInfos
+    .reduce((accumulator: WeatherDictionary, currentElement) => {
+      const day = currentElement.day.toISOString();
+      const time = currentElement.time.toISOString();
 
-    case ACTION_KEYS.FLUX_RETRIEVE_SUCCESS: { // todo remove code doubling from recducers:
-      const dayInfos = getDayInfos(action.by);
-
-      const dates: WeatherDictionary = dayInfos
-        .reduce((accumulator: WeatherDictionary, currentElement) => {
-          const day = currentElement.day.toISOString();
-          const time = currentElement.time.toISOString();
-
-          return { ...accumulator,
-            [day]: {
-              ...accumulator[day],
-              [time]: {
-                  ...(accumulator[day] || {})[time],
-                  Flux: currentElement.value
-                }
+      return { ...accumulator,
+        [day]: {
+          ...accumulator[day],
+          [time]: {
+              ...(accumulator[day] || {})[time],
+              ...callback(currentElement.value)
             }
-          };
-        }, state.Weather);
-
-      return {
-        ...state,
-        IsFluxLoading: false,
-        Weather: dates,
-        AllDays: mergeDateArrays(state.AllDays, dayInfos.map(dateTime => dateTime.day)),
-        AllTimes: mergeDateArrays(state.AllTimes, dayInfos.map(dateTime => dateTime.time))
+        }
       };
-    }
+    }, state.Weather);
 
-
-    case ACTION_KEYS.CLOUDNESS_RETRIEVE_SUCCESS: { // todo remove code doubling from recducers:
-      const dayInfos = getDayInfos(action.by);
-
-      const dates: WeatherDictionary = dayInfos
-        .reduce((accumulator: WeatherDictionary, currentElement) => {
-          const day = currentElement.day.toISOString();
-          const time = currentElement.time.toISOString();
-
-          return { ...accumulator,
-            [day]: {
-              ...accumulator[day],
-              [time]: {
-                  ...(accumulator[day] || {})[time],
-                  Cloud: currentElement.value
-                }
-            }
-          };
-        }, state.Weather);
-
-      return {
-        ...state,
-        IsCloudsLoading: false,
-        Weather: dates,
-        AllDays: mergeDateArrays(state.AllDays, dayInfos.map(dateTime => dateTime.day)),
-        AllTimes: mergeDateArrays(state.AllTimes, dayInfos.map(dateTime => dateTime.time))
-      };
-    }
-
-    default:
-      return state;
-  }
+  return { ...state,
+    Weather: dates,
+    AllDays: mergeToDistinctDates(state.AllDays, dayInfos.map(dateTime => dateTime.day)),
+    AllTimes: mergeToDistinctDates(state.AllTimes, dayInfos.map(dateTime => dateTime.time))
+  };
 }
-
-
