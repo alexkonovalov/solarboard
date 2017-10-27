4db7e940a6df32adab211f35b8b5b5497db308b9
alexkonovalov <alvkonovalov@gmail.com>
2017-10-27 22:23:16 +0300

    feat(powearboard): styling fix

--- src/app/powerboard/powerboard.action.ts
+++ src/app/powerboard/powerboard.action.ts
@@ -1,35 +1,35 @@
 import { PowerInfo } from './powerboard.model';
 
 export enum ACTION_KEYS {
-    POWER_RETRIEVE = 'POWER_RETRIEVE',
-    POWER_RETRIEVE_SUCCESS = 'POWER_RETRIEVE_SUCCESS',
-    POWER_RETRIEVE_ERROR = 'POWER_RETRIEVE_ERROR',
+  POWER_RETRIEVE = 'POWER_RETRIEVE',
+  POWER_RETRIEVE_SUCCESS = 'POWER_RETRIEVE_SUCCESS',
+  POWER_RETRIEVE_ERROR = 'POWER_RETRIEVE_ERROR',
 }
 
 export interface RetrievePowerAction {
-    type: ACTION_KEYS.POWER_RETRIEVE;
+  type: ACTION_KEYS.POWER_RETRIEVE;
 }
 
 export interface RetrievePowerActionSuccess {
-    type: ACTION_KEYS.POWER_RETRIEVE_SUCCESS;
-    by: PowerInfo[];
+  type: ACTION_KEYS.POWER_RETRIEVE_SUCCESS;
+  by: PowerInfo[];
 }
 
 export interface RetrievePowerActionFail {
-    type: ACTION_KEYS.POWER_RETRIEVE_ERROR;
-    by: string;
+  type: ACTION_KEYS.POWER_RETRIEVE_ERROR;
+  by: string;
 }
 
 export const actionRetrievePowerSuccess = (by: PowerInfo[]): RetrievePowerActionSuccess => ({
-    type: ACTION_KEYS.POWER_RETRIEVE_SUCCESS,
-    by
+  type: ACTION_KEYS.POWER_RETRIEVE_SUCCESS,
+  by
 });
 
 export const actionRetrievePowerFail = (by: string): RetrievePowerActionFail => ({
-    type: ACTION_KEYS.POWER_RETRIEVE_ERROR,
-    by
+  type: ACTION_KEYS.POWER_RETRIEVE_ERROR,
+  by
 });
 
 export const actionRetrievePower = (): RetrievePowerAction => ({
-    type: ACTION_KEYS.POWER_RETRIEVE
+  type: ACTION_KEYS.POWER_RETRIEVE
 });
