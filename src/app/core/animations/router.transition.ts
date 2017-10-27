d60cb0462794c62163a247558bf66fb69a354468
alexkonovalov <alvkonovalov@gmail.com>
2017-10-28 01:54:48 +0300

    feat(app): reduce the size of logo, simplify the rout animation

--- src/app/core/animations/router.transition.ts
+++ src/app/core/animations/router.transition.ts
@@ -8,14 +8,14 @@ import {
   stagger
 } from '@angular/animations';
 
-export const ANIMATE_ON_ROUTE_ENTER = 'route-enter-staggered';
+export const ANIMATE_ON_ROUTE_ENTER = 'route-enter';
 
 export const routerTransition = trigger('routerTransition', [
   transition('* <=> *', [
     query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
       optional: true
     }),
-    query(':enter .' + ANIMATE_ON_ROUTE_ENTER, style({ opacity: 0 }), {
+     query(':enter .' + ANIMATE_ON_ROUTE_ENTER, style({ opacity: 0 }), {
       optional: true
     }),
     group([
@@ -24,7 +24,7 @@ export const routerTransition = trigger('routerTransition', [
         [
           style({ transform: 'translateY(-3%)', opacity: 0 }),
           animate(
-            '0.5s 0.5s ease-in-out',
+            '0.4s 0.1s ease-in-out',
             style({ transform: 'translateY(0%)', opacity: 1 })
           )
         ],
@@ -35,23 +35,12 @@ export const routerTransition = trigger('routerTransition', [
         [
           style({ transform: 'translateY(0%)', opacity: 1 }),
           animate(
-            '0.2s ease-in-out',
+            '0.1s ease-in-out',
             style({ transform: 'translateY(-3%)', opacity: 0 })
           )
         ],
         { optional: true }
       )
-    ]),
-    query(
-      ':enter .' + ANIMATE_ON_ROUTE_ENTER,
-      stagger(100, [
-        style({ transform: 'translateY(15%)', opacity: 0 }),
-        animate(
-          '0.5s ease-in-out',
-          style({ transform: 'translateY(0%)', opacity: 1 })
-        )
-      ]),
-      { optional: true }
-    )
+    ])
   ])
 ]);
